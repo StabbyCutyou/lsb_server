def68ef685f659f267a18654cfb5f857c05b2760
StabbyCutyou <smattkelly@gmail.com>
2025-02-09 10:54:58 -0500

    f

--- scripts/effects/visitant.lua
+++ scripts/effects/visitant.lua
@@ -15,11 +15,11 @@ reportTimeRemaining = function(player, effect)
     local messageParam = 0
     local nextTimeReport = 0
 
-    player:printToPlayer('------- reportTimeRemaining ---------')
-    player:printToPlayer(string.format('gameLogin: %s', player:getLocalVar('gameLogin')))
-    player:printToPlayer(string.format('isInAbysseaZone: %s', xi.abyssea.isInAbysseaZone(player)))
-    player:printToPlayer(string.format('finalCountdown: %s', player:getLocalVar('finalCountdown')))
-    player:printToPlayer('------- reportTimeRemaining ---------')
+    player:printToPlayer('------- reportTimeRemaining ---------','')
+    player:printToPlayer(string.format('gameLogin: %s', player:getLocalVar('gameLogin')), '')
+    player:printToPlayer(string.format('isInAbysseaZone: %s', xi.abyssea.isInAbysseaZone(player)), '')
+    player:printToPlayer(string.format('finalCountdown: %s', player:getLocalVar('finalCountdown')), '')
+    player:printToPlayer('------- reportTimeRemaining ---------','')
 
     -- All possible forms of TE will reset out of the final two minute warning,
     -- reset this here.
@@ -82,11 +82,11 @@ effectObject.onEffectGain = function(target, effect)
 
     target:setLocalVar('lastTimeUpdate', effect:getTimeRemaining() / 1000 + 1)
 
-    target:printToPlayer('------- onEffectGain ---------')
-    target:printToPlayer(string.format('gameLogin: %s', target:getLocalVar('gameLogin')))
-    target:printToPlayer(string.format('isInAbysseaZone: %s', xi.abyssea.isInAbysseaZone(target)))
-    target:printToPlayer(string.format('finalCountdown: %s', target:getLocalVar('finalCountdown')))
-    target:printToPlayer('------- onEffectGain ---------')
+    target:printToPlayer('------- onEffectGain ---------','')
+    target:printToPlayer(string.format('gameLogin: %s', target:getLocalVar('gameLogin')), '')
+    target:printToPlayer(string.format('isInAbysseaZone: %s', xi.abyssea.isInAbysseaZone(target)), '')
+    target:printToPlayer(string.format('finalCountdown: %s', target:getLocalVar('finalCountdown')), '')
+    target:printToPlayer('------- onEffectGain ---------','')
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -112,11 +112,11 @@ effectObject.onEffectLose = function(target, effect)
     local zoneID = target:getZoneID()
     local ID = zones[zoneID]
 
-    target:printToPlayer('------- onEffectLose ---------')
-    target:printToPlayer(string.format('gameLogin: %s', target:getLocalVar('gameLogin')))
-    target:printToPlayer(string.format('isInAbysseaZone: %s', xi.abyssea.isInAbysseaZone(target)))
-    target:printToPlayer(string.format('finalCountdown: %s', target:getLocalVar('finalCountdown')))
-    target:printToPlayer('------- onEffectLose ---------')
+    target:printToPlayer('------- onEffectLose ---------','')
+    target:printToPlayer(string.format('gameLogin: %s', target:getLocalVar('gameLogin')), '')
+    target:printToPlayer(string.format('isInAbysseaZone: %s', xi.abyssea.isInAbysseaZone(target)), '')
+    target:printToPlayer(string.format('finalCountdown: %s', target:getLocalVar('finalCountdown')), '')
+    target:printToPlayer('------- onEffectLose ---------','')
 
     if
         target:getLocalVar('gameLogin') == 0 and
